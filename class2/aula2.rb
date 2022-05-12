13d6a7490f6a6c569c1fd99f744a35c7e557f52f
Lucasms29 <covalske9@gmail.com>
2022-05-12 14:46:22 -0300

    code edit aula2

--- class2/aula2.rb
+++ class2/aula2.rb
@@ -1,3 +1,5 @@
+/Tipos primitivos/
+
 int = 10
 puts int
 
@@ -6,3 +8,30 @@ puts float
 
 strg = "olá"
 puts strg
+
+/Operações matemáticas/
+ /soma/
+a = 1 
+b= 2
+puts a + b
+
+
+ /subtração/
+a = 3
+b = 2
+puts a - b
+
+ /multiplicação/
+a = 3
+b = 4
+puts a * b
+
+ /divisão/
+a = 10
+b = 5
+puts a / b
+
+
+
+
+
