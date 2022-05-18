4723a9d8e7ed82a5b295c63cdf75716d01c894e9
Lucasms29 <covalske9@gmail.com>
2022-05-18 16:44:48 -0300

    git update calculadora

--- RubyTDD/specs/calculadora_spec.rb
+++ RubyTDD/specs/calculadora_spec.rb
@@ -2,18 +2,16 @@ require 'rspec'
 require_relative '../src/calculadora'
 
 describe('calculadora') do
+    calculadora = Calculadora.new
     it('Deve somar dois inteiros positivos') do
-        calculadora = Calculadora.new
         expect(calculadora.soma(4, 5)).to eq 9
     end
 
     it('Deve somar um número qualquer com 0') do
-        calculadora = Calculadora.new
         expect(calculadora.soma(9, 0)).to eq 9
     end
 
     it('Deve somar números negativos') do
-        calculadora = Calculadora.new
         expect(calculadora.soma(-9, -10)).to eq -19
     end
 
